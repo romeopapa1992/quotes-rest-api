e64b399960960e0293ccaa4452fa1a23fdce4b1b
Rafal <rafal.proski.programista@gmail.com>
2025-01-11 11:24:06 +0100

    environment variables

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: "quotes-rest-api",
+        script: "index.js",
+        env: {
+          NODE_ENV: "production",
+          SESSION_SECRET: "MYQUOTES",
+          PG_USER: "postgres",
+          PG_HOST: "localhost",
+          PG_DATABASE: "quotes",
+          PG_PASSWORD: "2beornot2be",
+          PG_PORT: "5432",
+        },
+      },
+    ],
+  };
+  
