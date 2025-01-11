f1ef921ac1d4e829b3e8540183c5aa94cfc847df
Rafal <rafal.proski.programista@gmail.com>
2025-01-11 15:47:39 +0100

    ecosystem.config added

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "quotes-rest-api",
+        script: "./index.js",
+        env_file: "./.env",
+        env: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
