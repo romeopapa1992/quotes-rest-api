cdc611903fdcce081ff11d6ec1ad8156f14a9a8a
Rafal <rafal.proski.programista@gmail.com>
2025-01-11 19:56:37 +0100

    config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,19 @@
 export default {
   apps: [
     {
-      name: "quotes-rest-api",
+      name: "quotes-rest-api", 
       script: "./index.js",
       env_file: "./.env",
+      cwd: "/home/rafal/quotes-rest-api",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "quotes-app-react", 
+      script: "npx",
+      args: "serve -s build -l 4173",
+      cwd: "/home/rafal/quotes-app-react",
       env: {
         NODE_ENV: "production",
       },
